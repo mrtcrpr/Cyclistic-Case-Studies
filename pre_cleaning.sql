77c6f3afdd5ddf9012a3372bf2db858cc21e815d
mrtcrpr <100564253+mrtcrpr@users.noreply.github.com>
2022-10-07 21:22:48 +0300

    Update pre_cleaning.sql

--- pre_cleaning.sql
+++ pre_cleaning.sql
@@ -3,7 +3,6 @@
  I created a new table with the same number of columns and the same column names as the datasets I have and ı named '[2021_combined_data]'.
  Then ı connected the 12 month datasets together using union all.
  */
- 
 INSERT INTO
   [2021_combined_data]
 SELECT
@@ -80,7 +79,6 @@ SELECT
   *
 FROM
   [2021_combined_data];
-
 /*
  When we run the select query, it returns 5,595,063 rows. Since we use union all, all rows in the tables we include will be merged.
  The columns in the [2021_combined_data] table will be pre-cleaned.
@@ -91,7 +89,6 @@ FROM
  The first column we need to check is ride_id. Each value in the ride_id column must be 16 characters. 
  Except for 16 character data, outliers are data. Also, each row must be unique.
  */
- 
 SELECT
   len(ride_id),
   COUNT(*)
@@ -104,20 +101,18 @@ SELECT
   COUNT(DISTINCT ride_id)
 FROM
   [2021_combined_data];
-
 --There are 457 rows that are not 16 characters, this data needs to be cleaned and each row has a unique value.
 
 /*
  RIDEABLE_TYPE
  Next column we need to check is rideable_type.
  */
- 
 SELECT
   DISTINCT rideable_type
 FROM
   [2021_combined_data];
-
-/*We come across 3 unique values. These are electric_bike, classic_bike and docked_bike.
+/*
+ We come across 3 unique values. These are electric_bike, classic_bike and docked_bike.
  classic_bike and docked_bike can basically be described as the same because both are not powered by electricity.
  The docked_bike needs to be converted to the classic_bike.
  */
@@ -128,7 +123,6 @@ FROM
  In this part, the journey will not be less than 1 minute and more than 1 day.
  Also there is a weird problem in here. The problem is some ended_at rows less than started_at rows.
  */
- 
 SELECT
   ride_id,
   started_at,
@@ -157,7 +151,6 @@ FROM
   [2021_combined_data]
 WHERE
   DATEDIFF(MINUTE, started_at, ended_at) > 1440;
-
 /*
  This rows should be deleted.
  Because this rows are inconsistent.
@@ -169,7 +162,6 @@ WHERE
  There were 3 rows in the table where the start_station_name column was null but the start_station_id column was not null.
  Also there is some 'Temp' values trailing of the station name columns. They need to be deleted.
  */
- 
 SELECT
   start_station_name,
   start_station_id
@@ -196,14 +188,12 @@ FROM
 WHERE
   start_station_name LIKE '%temp%'
   OR end_station_name LIKE '%temp%';
-
 --Regardless of the rideable_type, all rows with at least 1 null value should be deleted.
 
 /*
  LAT and LNG
  Other columns to examine are the latitude and longitude columns.
  */
- 
 SELECT
   *
 FROM
@@ -213,55 +203,14 @@ WHERE
   OR start_lng IS NULL
   OR end_lat IS NULL
   OR end_lng IS NULL;
-
 --Since these latitude and longitude values will be mapped later, null values need to be eliminated.
 
 /*
  MEMBER_CASUAL
  The next column to be evaluated is the member_casual column and must contain two values. These are member and casual.
  */
- 
 SELECT
   DISTINCT member_casual
 FROM
   [2021_combined_data];
-
 --No values to delete were found in this column.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
